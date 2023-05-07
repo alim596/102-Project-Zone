56a412744b711fe486bcb53f7dca83dca6020e47
Abdulaleem Altarsha <abdulaleem@ug.bilkent.edu.tr>
2023-05-07 12:16:08 +0300

    Home page implemented (without onClick expansion)

---
+++ app/src/main/java/com/example/navogation_with_pages/ZonesRecViewAdapter.java
@@ -0,0 +1,75 @@
+package com.example.navogation_with_pages;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ZonesRecViewAdapter extends RecyclerView.Adapter<ZonesRecViewAdapter.ViewHolder> {
+
+    private ArrayList<Zone> zones = new ArrayList<>();
+
+    public ZonesRecViewAdapter() {
+    }
+
+    //This method creates an instance of the ViewHolder class for every item in our recyclerView.
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        /*
+        * The first parameter gets the xml file to inflate, the parent we want to attach the view to, and a boolean.
+        * The boolean checks if we want to attach the view to the parent. Making it on would be redundant since we
+        * already passed the parent. If we are not sure what the parent will be, we can pass null and true instead.
+        * */
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.zones_list_item, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.name.setText(zones.get(position).getName());
+        holder.quota.setText(zones.get(position).getName());
+        holder.date.setText(zones.get(position).getName());
+        holder.details.setText(zones.get(position).getName());
+        //holder.image.setImageDrawable(zones.get(position).getImageUrl());
+    }
+
+    @Override
+    public int getItemCount() {
+        return zones.size();
+    }
+
+    public void setZones(ArrayList<Zone> zones) {
+        this.zones = zones;
+        notifyDataSetChanged();
+    }
+
+
+    //This class gives objects that can hold CardView items to add to the home page
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView name;
+        private TextView quota;
+        private TextView date;
+        private TextView details;
+        private ImageView image;
+        private CardView zoneCrdView;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            zoneCrdView = itemView.findViewById(R.id.zoneCrdView);
+            name = itemView.findViewById(R.id.name);
+            quota = itemView.findViewById(R.id.quota);
+            date = itemView.findViewById(R.id.date);
+            details = itemView.findViewById(R.id.details);
+            image = itemView.findViewById(R.id.image);
+        }
+    }
+}
--- app/src/main/java/com/example/navogation_with_pages/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/navogation_with_pages/ui/home/HomeFragment.java
@@ -4,29 +4,60 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.navogation_with_pages.R;
+import com.example.navogation_with_pages.Zone;
+import com.example.navogation_with_pages.ZonesRecViewAdapter;
 import com.example.navogation_with_pages.databinding.FragmentHomeBinding;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private RecyclerView zonesRecView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        zonesRecView = view.findViewById(R.id.zonesRecView);
+
+        ArrayList<Zone> zones = new ArrayList<Zone>();
+        zones.add(new Zone("Abdulaleem", "0/5", new Date(), "Lets go to the Mall together and have some fun!!",
+                "Kent Park", "Private", "mall.png"));
+        zones.add(new Zone("Orhun", "3/5", new Date(), "We are going to play Basketball and we need 5 more people. You can join if you have ever touched a basketball before.",
+                "Main Campus Sports Hall", "public", "basketball.png"));
+        zones.add(new Zone("Irmak", "1/5", new Date(), "We will be drinking coffee together in Coffee break. Only for CS students!",
+                "Coffee Break", "Private", "coffeeBreak.png"));
+        zones.add(new Zone("Fazlı", "4/5", new Date(), "We are planning a football game and anyone is welcomed!",
+                "Main Campus Football Field", "public", "football.png"));
+
+        ZonesRecViewAdapter adapter = new ZonesRecViewAdapter();
+        adapter.setZones(zones);
+
+        zonesRecView.setAdapter(adapter);
+        zonesRecView.setLayoutManager(new LinearLayoutManager(this.getContext()));
     }
 
     @Override
